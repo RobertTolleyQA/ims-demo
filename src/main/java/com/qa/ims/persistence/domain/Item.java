5d36ad56722f612173058f123d7c316c05241537
RobTolley <rtolley@qa.com>
2021-01-13 10:54:02 +0000

    constructors, getters & setters

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -1,5 +1,61 @@
 package com.qa.ims.persistence.domain;
 
 public class Item {
+	
+	private Long id;
+	private String itemName;
+	private String itemValue;
+	
+	
+	public Item(Long id, String itemName, String itemValue) {
+		super();
+		this.id = id;
+		this.itemName = itemName;
+		this.itemValue = itemValue;}
+
+
+	public Item(String itemName, String itemValue) {
+		super();
+		this.itemName = itemName;
+		this.itemValue = itemValue;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getItemName() {
+		return itemName;
+	}
+
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+
+	public String getItemValue() {
+		return itemValue;
+	}
+
+
+	public void setItemValue(String itemValue) {
+		this.itemValue = itemValue;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
