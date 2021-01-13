d3265e6c420c6d28ce3af4e40138471b51eaa4ef
RobTolley <rtolley@qa.com>
2021-01-13 17:23:36 +0000

    constructor+get/set+toString for Orders

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -1,5 +1,47 @@
 package com.qa.ims.persistence.domain;
 
 public class Order {
+	
+	private Long custID;
+	private Long orderID;
+	
+	
+	public Order(long custID, long orderID) {
+		super();
+		this.custID = custID;
+		this.orderID = orderID;
+	}
+
+
+	public long getCustID() {
+		return custID;
+	}
+
+
+	public void setCustID(long custID) {
+		this.custID = custID;
+	}
+
+
+	public long getOrderID() {
+		return orderID;
+	}
+
+
+	public void setOrderID(long orderID) {
+		this.orderID = orderID;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Order custID=" + custID + ", orderID=" + orderID;
+	} 
+	
+	
+	
+	
+	
+	
 
 }
