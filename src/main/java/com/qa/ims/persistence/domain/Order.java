afe9355e4df8bd0b2ba71f26a453ccd899cff9f3
RobTolley <rtolley@qa.com>
2021-01-15 10:49:41 +0000

    moved orderline over to orders

--- src/main/java/com/qa/ims/persistence/domain/Order.java
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -3,9 +3,25 @@ package com.qa.ims.persistence.domain;
 public class Order {
 	
 	private Long custID;
+	private Long id;
 	private Long orderID;
+	private Long itemID;
+	private Integer quantity;
 	
 	
+	
+	//orderline constructor
+	
+	public Order(Long id, Long orderID, Long itemID, Integer quantity) {
+		super();
+		this.id = id;
+		this.orderID = orderID;
+		this.itemID = itemID;
+		this.quantity = quantity;
+	}
+
+
+	//order constructor
 	public Order(long orderID, long custID) {
 		super();
 		this.orderID = orderID;
@@ -40,12 +56,48 @@ public class Order {
 	public void setOrderID(long orderID) {
 		this.orderID = orderID;
 	}
-
-
+	
+	
 	@Override
 	public String toString() {
-		return "Order orderID=" + orderID + ", custID=" + custID;
+		return "Order [id=" + id + ", orderID=" + orderID + ", itemID=" + itemID + ", quantity=" + quantity + "]";
+	}
+
+
+	
+
+	//orderline getters & setters
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public Long getItemID() {
+		return itemID;
+	}
+
+
+	public void setItemID(Long itemID) {
+		this.itemID = itemID;
+	}
+
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
 	} 
+	
+	
 	
 	
 	
