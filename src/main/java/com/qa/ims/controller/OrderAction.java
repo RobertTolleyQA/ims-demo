943bed3ded786159b0e776952a09896ed263c6c9
RobTolley <rtolley@qa.com>
2021-01-19 09:34:58 +0000

    orderAction added

---
+++ src/main/java/com/qa/ims/controller/OrderAction.java
@@ -0,0 +1,60 @@
+package com.qa.ims.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.utils.Utils;
+
+/**
+ * Action is a collection of commands which are used to determine the type of
+ * function to apply to an entity.
+ *
+ */
+public enum OrderAction {
+	CREATE("To save a new item into the database"), READ("To read an item from the database"),
+	UPDATE("To change an item already in the database"), DELETE("To remove an item from the database"),
+	RETURN("To return to domain selection");
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private String description;
+
+	OrderAction(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * Describes the action
+	 */
+	public String getDescription() {
+		return this.name() + ": " + this.description;
+	}
+
+	/**
+	 * Prints out all possible actions
+	 */
+	public static void printActions() {
+		for (Action action : Action.values()) {
+			LOGGER.info(action.getDescription());
+		}
+	}
+
+	/**
+	 * Gets an action based on a users input. If user enters a non-specified
+	 * enumeration, it will ask for another input.
+	 * 
+	 * @return Action type
+	 */
+	public static Action getAction() {
+		Action action = null;
+		do {
+			try {
+				action = Action.valueOf(Utils.getInstance().getInput().toUpperCase());
+			} catch (IllegalArgumentException e) {
+				LOGGER.error("Invalid selection please try again");
+			}
+		} while (action == null);
+		return action;
+	}
+
+}
