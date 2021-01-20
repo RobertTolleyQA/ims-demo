b25bccd5595b77ee67433c548748ebe654a38ed1
RobTolley <rtolley@qa.com>
2021-01-20 14:22:13 +0000

    added item hashcode + equals

--- src/main/java/com/qa/ims/persistence/domain/Item.java
+++ src/main/java/com/qa/ims/persistence/domain/Item.java
@@ -56,6 +56,42 @@ public class Item {
 		return "Item id=" + id + ", item name=" + itemName + ", item value= £" + itemValue;
 	}
 	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemName == null) ? 0 : itemName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((itemValue == null) ? 0 : itemValue.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (itemName == null) {
+			if (other.itemName != null)
+				return false;
+		} else if (!itemName.equals(other.itemName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (itemValue == null) {
+			if (other.itemValue != null)
+				return false;
+		} else if (!itemValue.equals(other.itemValue))
+			return false;
+		return true;
+	}
 	
 	
 	
