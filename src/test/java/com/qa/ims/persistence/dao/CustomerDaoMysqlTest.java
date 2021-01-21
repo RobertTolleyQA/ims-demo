4e4eaf31bec920b09ffa66629fe2bba6439ac3ff
RobTolley <rtolley@qa.com>
2021-01-21 15:29:27 +0000

    most customerdao tests done;

--- src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
+++ src/test/java/com/qa/ims/persistence/dao/CustomerDaoMysqlTest.java
@@ -21,14 +21,24 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.services.CustomerServices;
 import com.qa.ims.utils.DBUtils;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CustomerDaoMysqlTest {
+	
+	@Mock
+	private Dao<Customer> customerDao;
+	
+	@InjectMocks
+	private CustomerDaoMysql custDao;
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
@@ -60,16 +70,17 @@ public class CustomerDaoMysqlTest {
 		String fn = "rob";
 		String sn = "bob";
 		Customer customer = new Customer(fn, sn);
-		Customer savedCustomer = new Customer(1L, fn, sn);
+		Customer savedCustomer = new Customer(3L, fn, sn);
 		customer = custsql.create(customer);
 		assertEquals(savedCustomer, customer);
 	}
 	
-	@Ignore
+
 	@Test
 	public void readAllTest() {
 		CustomerDaoMysql custsql = new CustomerDaoMysql();
-		Customer customer = new Customer(1L, "rob", "bob");
+		Customer customer = custsql.create(new Customer(1L, "rob", "bob"));
+		
 		List<Customer> customers = new ArrayList<>();
 		List<Customer> savedCustomers = new ArrayList<>();
 		savedCustomers.add(customer);
@@ -80,7 +91,8 @@ public class CustomerDaoMysqlTest {
 	@Test
 	public void readCustomerTest() {
 		CustomerDaoMysql custsql = new CustomerDaoMysql();
-		Customer customer = new Customer("rob", "bob");
+//		Customer customer = new Customer("rob", "bob");
+		Customer customer = custsql.create(new Customer("rob","bob"));
 		Customer savedCustomer = new Customer(1L, "rob", "bob");
 		customer = custsql.readCustomer(1L);
 		assertEquals(savedCustomer, customer);
@@ -98,5 +110,27 @@ public class CustomerDaoMysqlTest {
 		assertEquals(savedCustomer, customer);
 
 	}
+	
+	@Test
+	public void modelFromResultSetOrderTest() throws SQLException {
+		Connection connection = DBUtils.getInstance().getConnection();
+		Statement statement = connection.createStatement();
+		CustomerDaoMysql custsql = new CustomerDaoMysql();
+		OrderDAO orderDao = new OrderDAO();
+		Long id = 1L;
+		Order order1 = new Order(1L, 0);
+		orderDao.create(order1);
+		Order savedOrder = new Order(1L,1L);
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM orders ORDER BY orderID DESC LIMIT 1");
+		resultSet.next();
+		
+		Order order = custsql.modelFromResultSetOrder(resultSet);
+		assertEquals(savedOrder, order);
+		
+	}
+
+		
+		
+	
 
 }
