2600b718f030abc9f13a456b74e684f7bdacf2c3
RobTolley <rtolley@qa.com>
2021-01-21 16:53:02 +0000

    order testing;

--- src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
+++ src/test/java/com/qa/ims/persistence/dao/OrderDAOTest.java
@@ -8,6 +8,9 @@ import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.utils.DBUtils;
 
@@ -24,16 +27,21 @@ public class OrderDAOTest {
 	public static void setup() {
 		DBUtils.getInstance(DB_USER, DB_PASS);
 	}
-	@Ignore
+
 	@Test
 	public void readAllTest() {
 		OrderDAO orderdao= new OrderDAO();
-		Order order = orderdao.create(new Order(1L, 0));
-		Order orderline = orderdao.createOrderline(order);
+		CustomerDaoMysql custsql = new CustomerDaoMysql();
+		ItemDAO itemdao = new ItemDAO();
+		custsql.create(new Customer("rob", "bob"));
+		itemdao.create(new Item("hat", 10.5));
+		orderdao.create(new Order(1L, 0));
+		Order order = new Order(1L, 1L, 5);
+		orderdao.createOrderline(order);
 
 		List<Order> orders = new ArrayList<>();
 		List<Order> savedOrders = new ArrayList<>();
-		savedOrders.add(orderline);
+		savedOrders.add(order);
 		orders.addAll(orderdao.readAll());
 		assertEquals(savedOrders, orders);
 
