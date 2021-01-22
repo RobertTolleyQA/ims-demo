934d3bc372063170b469d0bc6c9c68cd7f2b9c79
RobTolley <rtolley@qa.com>
2021-01-22 13:03:16 +0000

    added target folder

---
+++ target/test-classes/sql-schema.sql
@@ -0,0 +1,3 @@
+create database if not exists ims_test;
+drop table if exists ims.customers;
+create table ims_test.customers(id int primary key auto_increment, first_name varchar(40), surname varchar(40));
